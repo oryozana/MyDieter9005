34d2cf1d37de353f8cbf861fabf728c132596a59
oryozana <oryozana@gmail.com>
2022-10-28 15:51:24 +0300

    Small change...

--- app/src/main/java/com/example/mydieter9005/Song.java
+++ app/src/main/java/com/example/mydieter9005/Song.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Song extends AppCompatActivity implements Serializable {
     private static ArrayList<Song> songs = new ArrayList<Song>(); // Contain every song basic info.
-    private String name;
+    private final String name;
     private boolean isPlaying;
     private final int id;
 
