b982476407f0617b030ddc723527c548eeba93d6
oryozana <oryozana@gmail.com>
2022-11-06 12:42:23 +0200

    Change password added inside UserInfoScreen screen.

--- app/src/main/java/com/example/mydieter9005/UserInfoScreen.java
+++ app/src/main/java/com/example/mydieter9005/UserInfoScreen.java
@@ -12,9 +12,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -27,12 +35,16 @@ public class UserInfoScreen extends AppCompatActivity implements View.OnClickLis
     private MediaPlayer mediaPlayer;
     private VideoView videoView;
 
-    Button btSendToProfilePictureSelection;
+    Button btSendToProfilePictureSelection, btChangePassword;
+    EditText etGetOldPassword, etGetNewPassword;
     ImageView ivProfilePicture;
     LinearLayout linearLayout;
 
     Song activeSong = Song.getSongs().get(0);
     User user = User.getCurrentUser();
+
+    FirebaseDatabase usersDb;
+    DatabaseReference databaseReference;
 
     FileInputStream is;
     InputStreamReader isr;
@@ -56,6 +68,11 @@ public class UserInfoScreen extends AppCompatActivity implements View.OnClickLis
 
         btSendToProfilePictureSelection = (Button) findViewById(R.id.btSendToProfilePictureSelection);
         btSendToProfilePictureSelection.setOnClickListener(this);
+        btChangePassword = (Button) findViewById(R.id.btChangePassword);
+        btChangePassword.setOnClickListener(this);
+
+        etGetOldPassword = (EditText) findViewById(R.id.etGetOldPassword);
+        etGetNewPassword = (EditText) findViewById(R.id.etGetNewPassword);
 
         implementSettingsData();
         initiateVideoPlayer();
@@ -65,6 +82,84 @@ public class UserInfoScreen extends AppCompatActivity implements View.OnClickLis
     public void sendToProfilePictureSelection(){
         me.setClass(UserInfoScreen.this, ProfilePictureSelection.class);
         startActivity(me);
+    }
+
+    public void changePassword(){
+        boolean passTests = passChangePasswordTests();
+
+        if(passTests){
+            String userPassword = User.getCurrentUser().getPassword();
+            if(userPassword.equals(etGetOldPassword.getText().toString())){
+                User.getCurrentUser().setPassword(etGetNewPassword.getText().toString());
+                saveUserInFirebase(User.getCurrentUser());
+                Toast.makeText(this, "Password successfully changed.", Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(this, "Wrong password.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean passChangePasswordTests(){
+        boolean passTests = true;
+
+        if(etGetOldPassword.getText().toString().replaceAll(" ", "").equals("") && passTests){
+            Toast.makeText(this, "Enter your old password!", Toast.LENGTH_SHORT).show();
+            passTests = false;
+        }
+
+        if(etGetNewPassword.getText().toString().replaceAll(" ", "").equals("") && passTests){
+            Toast.makeText(this, "Enter new password!", Toast.LENGTH_SHORT).show();
+            passTests = false;
+        }
+        else{
+            if(etGetNewPassword.getText().toString().length() < 4 && passTests){
+                Toast.makeText(this, "Password should be at least 4 characters wide!", Toast.LENGTH_SHORT).show();
+                passTests = false;
+            }
+        }
+
+        return passTests;
+    }
+
+    public void saveUserInFirebase(User user){
+        usersDb = FirebaseDatabase.getInstance();
+        databaseReference = usersDb.getReference("users");
+        databaseReference.child(user.getUsername()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                User.setCurrentUser(user);
+            }
+        });
+    }
+
+    public void getUserFromFirebaseDatabase(String username, String entered_password){
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.child(username).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    if(task.getResult().exists()){
+                        DataSnapshot dataSnapshot = task.getResult();
+                        String username = dataSnapshot.getKey();
+                        String password = String.valueOf(dataSnapshot.child("password").getValue());
+
+                        if(entered_password.equals(password)){
+                            double targetCalories = Double.parseDouble(String.valueOf(dataSnapshot.child("currentPlan").child("targetCalories").getValue()));
+                            double targetProteins = Double.parseDouble(String.valueOf(dataSnapshot.child("currentPlan").child("targetProteins").getValue()));
+                            double targetFats = Double.parseDouble(String.valueOf(dataSnapshot.child("currentPlan").child("targetFats").getValue()));
+                            Plan plan = new Plan(targetCalories, targetProteins, targetFats);
+
+                            String email = String.valueOf(dataSnapshot.child("email").getValue());
+                            double startingWeight = Double.parseDouble(String.valueOf(dataSnapshot.child("startingWeight").getValue()));
+                            int profilePictureId = Integer.parseInt(String.valueOf(dataSnapshot.child("profilePictureId").getValue()));
+                            User.setCurrentUser(new User(username, password, email, startingWeight, plan, profilePictureId));
+                        }
+                    }
+                    else
+                        Toast.makeText(UserInfoScreen.this, "Not the correct password.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public String getFileData(String fileName){
@@ -202,6 +297,9 @@ public class UserInfoScreen extends AppCompatActivity implements View.OnClickLis
 
         if(viewId == btSendToProfilePictureSelection.getId())
             sendToProfilePictureSelection();
+
+        if(viewId == btChangePassword.getId())
+            changePassword();
     }
 
     @Override
